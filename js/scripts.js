16397f6637b487f5b43ec80bbeb9beda27e2a721
s-odhiambo <samuelangienda1998@gmail.com>
2019-09-29 19:41:55 +0300

    add stylings to project one, two, three, four, five,six,seven and eight

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
      $("#prod-pic").hide(1500).toggle();
      $(".prod-a").slideDown();
    });
-   
+
    $(".prod-a").click(function(){
     $("#prod-pic").slideDown();
     $(".prod-a").hide();
@@ -31,3 +31,69 @@ $(document).ready(function(){
 
  });
 
+
+ $(document).ready(function(){
+   $(".pro1").mouseover(function(){
+     $(".project1").show();
+   }).mouseout(function(){
+     $(".project1").hide();
+   });
+ });
+
+ $(document).ready(function(){
+  $(".pro2").mouseover(function(){
+    $(".project2").show();
+  }).mouseout(function(){
+    $(".project2").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".pro3").mouseover(function(){
+    $(".project3").show();
+  }).mouseout(function(){
+    $(".project3").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".pro4").mouseover(function(){
+    $(".project4").show();
+  }).mouseout(function(){
+    $(".project4").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".pro5").mouseover(function(){
+    $(".project5").show();
+  }).mouseout(function(){
+    $(".project5").hide();
+  });
+});
+
+
+$(document).ready(function(){
+  $(".pro6").mouseover(function(){
+    $(".project6").show();
+  }).mouseout(function(){
+    $(".project6").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".pro7").mouseover(function(){
+    $(".project7").show();
+  }).mouseout(function(){
+    $(".project7").hide();
+  });
+});
+
+
+$(document).ready(function(){
+  $(".pro8").mouseover(function(){
+    $(".project8").show();
+  }).mouseout(function(){
+    $(".project8").hide();
+  });
+});
